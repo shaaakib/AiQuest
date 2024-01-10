924436446c7871637524a5dcc43bcbfa845fb8b2
shaaakib <>
2024-01-10 18:50:20 +0600

    using bootstrap

--- studymart/Deep_Learning/views.py
+++ studymart/Deep_Learning/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def deep_learning(request):
-    return render(request,'deep_learning.html')
+    return render(request,'deep_learning/deep_learning.html')
 
