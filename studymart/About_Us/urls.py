68653bce1fbf67d6862aff69a9e097809b87fe3d
shaaakib <>
2024-01-11 15:05:25 +0600

    Show Database Table Data to User

--- studymart/About_Us/urls.py
+++ studymart/About_Us/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('a/', views.about_us, name='about'),
+    path('t/', views.teachers_info),
 ]
--- studymart/About_Us/views.py
+++ studymart/About_Us/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from About_Us.models import Teacher
 
 # Create your views here.
 def about_us(request):
     return render(request, 'about/about.html')
+
+def teachers_info(request):
+    teach = Teacher.objects.all()
+    return render(request,'about/t.html', {'thr':teach})
