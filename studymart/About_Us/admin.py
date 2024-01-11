d361a0d774cb534e8fa4789875e4024dbc3c6f82
shaaakib <>
2024-01-11 15:44:47 +0600

    Create Superuser & Register Model Class or Clas

--- studymart/About_Us/admin.py
+++ studymart/About_Us/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from About_Us.models import Teacher
+class TeacherAdmin(admin.ModelAdmin):
+ list_display=('id', 'tid', 'tname', 'temail')
+#Register your models here.
+admin.site.register(Teacher, TeacherAdmin)
