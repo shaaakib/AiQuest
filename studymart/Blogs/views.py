684ccd7e0a6c8bfc1694165ad5a6b03fd0651a5b
shaaakib <>
2024-01-12 09:50:16 +0600

    work with GET, POST method & Cross-Site Request Forgery (CSRF)

--- studymart/Blogs/views.py
+++ studymart/Blogs/views.py
@@ -8,5 +8,12 @@ def blog1(request):
     return render(request, 'blogs/blogs.html')
 
 def showformsdata(request):
-    fm = TeacherRegistration()
+    if request.method == 'POST':
+        fm = TeacherRegistration(request.POST)
+        print(fm)
+        print('This is post statement')
+        print(fm.cleaned_data)
+    else:
+     fm = TeacherRegistration()
+     print('This is get statement')
     return render(request, 'blogs/forms.html', {'form':fm})
