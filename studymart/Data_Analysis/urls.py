8c7947778c21ab94f89a1957f67b4e78cd7d294c
shaaakib <>
2024-01-12 11:06:43 +0600

    Create Class Based View and it Works

--- studymart/Data_Analysis/urls.py
+++ studymart/Data_Analysis/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('analysis/', views.data_analysis),
+    path('class/', views.DataAnalysis.as_view()),
 ]
--- studymart/Data_Analysis/views.py
+++ studymart/Data_Analysis/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 def data_analysis(request):
-    return render(request, 'data_analysis.html')
+    return render(request, 'data_analysis/data_analysis.html')
+
+class DataAnalysis(View):
+    def get(self, request):
+        return render(request, 'data_analysis/class.html')
+
+
+
